98291a5339d7fe0a5003ef4d57ff0be0ba31ab7d
jeffasseur <r0846107@student.thomasmore.be>
2022-05-15 20:35:03 +0200

    feat: setup sass for vue project

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Homescreen from './components/Home.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="RUNE logo" src="./assets/thorchaine.png" style="width: 10%;" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
