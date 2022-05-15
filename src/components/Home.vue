676357cdda4cad929888c48223c50b3e80c8ff24
jeffasseur <r0846107@student.thomasmore.be>
2022-05-15 20:33:17 +0200

    feat: install vue routing for single page routing

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Jef Fasseur</h1>
+        <p>First component, this will be the homescreen</p>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: #2c3e50;
+}
+</style>
